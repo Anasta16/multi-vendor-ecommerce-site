cff2cc481b8be53278c93c41544ead03e65edebe
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-14 15:46:25 -0400

    installed tailwind css

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Changing
+      </h1>
+    </div>
+  );
+}
+
+export default App;
