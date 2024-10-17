c12dc1aef540faba2af1927087252df742bd70f5
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-17 16:43:46 -0400

    successfully hooked up backend server to frontend and created authRoutes and authControllers files for admin-login page, encountered cors error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+app.use('/api', require('./routes/authRoutes'))
 
 app.get('/', (req, res) => res.send('My backend'))
 const port = process.env.PORT
