49a8438121fc761f08008993f9ca8ac64a0ecb7b
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-17 11:48:56 -0400

    added Provider to index.js and called the store from index.js/store inside the store folder, redux chrome extension tool now working and displaying default state for admin/login page

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -4,6 +4,14 @@ import rootReducer from "./rootReducer";
 
 const store = configureStore({
 
-    reducer : rootReducer
+    reducer : rootReducer,
+    middleware : getDefaultMiddleware => {
+        return getDefaultMiddleware({
+            serializableCheck : false
+        })
+    },
+    devTools : true
 
 })
+
+export default store;
