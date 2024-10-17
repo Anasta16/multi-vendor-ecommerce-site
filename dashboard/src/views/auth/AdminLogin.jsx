754507097dc7ae7c4b5fc84a2a0858a15a99b347
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-17 11:18:18 -0400

    created AdminLogin page, added state and functionality

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,8 +1,6 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
-import { FaGoogle, FaFacebook } from "react-icons/fa";
 
-const Login = () => {
+const AdminLogin = () => {
 
     const [state, setState] = useState({
         email: "",
@@ -27,12 +25,16 @@ const Login = () => {
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
             <div className='w-[350px] text-[#ffffff] p-2'>
                 <div className='bg-[#6f68d1] p-4 rounded-md'>
-                    <h2 className='text-xl mb-3 font-bold'>
-                        Welcome to Ecommerce
-                    </h2>
-                    <p className='text-sm mb-3 font-medium'>
-                        Please sign into your account
-                    </p>
+
+                <div className="h-[70px] flex justify-center items-center">
+                    <div className="w-[180px] h-[50px]">
+                        <img 
+                            className="w-full h-full"
+                            src="http://localhost:3000/images/logo.png" 
+                            alt="admin-image" 
+                        />
+                    </div>
+                </div>
 
                     <form onSubmit={submit}>
                         <div className='flex flex-col w-full gap-1 mb-3'>
@@ -63,32 +65,8 @@ const Login = () => {
                         </div>
 
                         <button className="bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-                            Sign In
+                            Login
                         </button>
-
-                        <div className='flex items-center mb-3 gap-3 justify-center'>
-                            <p>
-                                Don't have an account? <Link className='font-bold' to="/register">Sign Up</Link> 
-                            </p>
-                        </div>
-
-                        <div className='w-full flex justify-center items-center mb-3'>
-                            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
-                            <div className='w-[10%] flex justify-center items-center'>
-                                <span className='pb-1'>Or</span>
-                            </div>
-                            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
-                        </div>
-
-                        <div className='flex justify-center items-center gap-3'>
-                            <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
-                                <span><FaGoogle /></span>
-                            </div>
-                            <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
-                                <span><FaFacebook /></span>
-                            </div>
-                        </div>
-
                     </form>
                 </div>
             </div>        
@@ -96,4 +74,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default AdminLogin;
