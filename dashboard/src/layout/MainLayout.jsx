a36742105047fa556d031e5a0615da9dfd94f0fa
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-21 13:01:32 -0400

    designed and provided functionality to hide or show sidebar depending on the screen size, added styling to component

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
     return (
         <div className='bg-[#cdcae9] w-full min-h-screen' >
-            <Header />
-            <Sidebar />
-            
+            <Header 
+                showSidebar={showSidebar} 
+                setShowSidebar={setShowSidebar} 
+            />
+            <Sidebar 
+                showSidebar={showSidebar} 
+                setShowSidebar={setShowSidebar} 
+            />
+
             <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all' >
                 <Outlet />
             </div>
