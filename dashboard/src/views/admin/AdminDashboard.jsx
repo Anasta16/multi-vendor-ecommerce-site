1237c76b7b4036fc1bd08505aa025ae4ed73d07c
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-10-22 10:44:14 -0400

    created initial static chart using apexcharts, styled chart to be responsive to different screen sizes

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,8 +1,72 @@
 import React from "react";
 import { MdCurrencyExchange, MdProductionQuantityLimits } from 'react-icons/md';
 import { FaCartArrowDown, FaUsers } from 'react-icons/fa';
+import Chart from 'react-apexcharts';
 
 const AdminDashboard = () => {
+
+    const state = {
+        series : [
+            {
+                name : "Orders",
+                data : [23, 34, 45, 56, 76, 34, 23, 76, 87, 78, 34, 45]
+            },
+            {
+                name : "Revenue",
+                data : [67, 39, 45, 56, 90, 56, 23, 56, 87, 78, 67, 78]
+            },
+            {
+                name : "Sellers",
+                data : [34, 39, 56, 56, 80, 67, 23, 56, 98, 78, 45, 56]
+            }
+        ],
+        options : {
+            color : ['#181ee8', '#181ee8'],
+            plotOptions : {
+                radius : 30
+            },
+            chart : {
+                background : 'transparent',
+                foreColor : '#d0d2d6'
+            },
+            dataLevels : {
+                enabled : false,
+            },
+            strock : {
+                show : true,
+                curve : ['smooth', 'straight', 'stepline'],
+                lineCap : 'butt',
+                colors : '#f0f0f0',
+                width : .5,
+                dashArray : 0
+            },
+            xaxis : {
+                categories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            legend : {
+                position : 'top'
+            },
+            responsive : [
+                {
+                    breakpoint : 565,
+                    yaxis : {
+                        categories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    },
+                    options : {
+                        plotOptions : {
+                            bar : {
+                                horizontal : true
+                            }
+                        },
+                        chart : {
+                            height : "550px"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
     return (
         <div className='px-2 md:px-7 py-5'>
            <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7'>
@@ -57,12 +121,29 @@ const AdminDashboard = () => {
                         <FaCartArrowDown
                             className="text-[#fae8e8] shadow-lg"
                         />
-
                     </div>
                 </div>
+           </div>
 
+
+           <div className='w-full flex flex-wrap mt-7 '>
+                <div className='w-full lg:w-7/12 lg:pr-3'>
+                    <div className='w-full bg-[#6a5fdf] p-4 rounded-md'>
+                        <Chart 
+                            options={state.options}
+                            series={state.series}
+                            type='bar'
+                            height={350}
+                        />
+                    </div>
+
+                </div>
 
            </div>
+
+
+
+
         </div>
     )
 }
